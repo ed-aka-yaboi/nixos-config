3636d13380ad938b68aeed62dcf06dd24e9ed6a6
sigkill <blakat360@gmail.com>
2026-09-13 18:01:45 +0100

    latest

--- home/editor/emacs.nix
+++ home/editor/emacs.nix
@@ -3,26 +3,26 @@
   home =
     let
       npmGlobal = "${config.home.homeDirectory}/.npm-global";
+      # needed to build some lisp packages add ons
+      extraLibPaths = pkgs.lib.makeLibraryPath [
+        pkgs.openssl
+      ];
     in
     {
       # allow `npm install -g` to install into ~/.npm-global without sudo/nix
-      sessionVariables.NPM_CONFIG_PREFIX = npmGlobal;
+      sessionVariables = {
+        NPM_CONFIG_PREFIX = npmGlobal;
+        LD_LIBRARY_PATH = extraLibPaths;
+      };
       sessionPath = [ "${npmGlobal}/bin" ];
 
       packages = with pkgs; [
-        autoconf
         editorconfig-core-c
-        gtk3
-        libgccjit
-        librsvg
-        libwebp
         multimarkdown
         nodejs
         pkg-config
         shellcheck
         shfmt
-        source-code-pro # spacemacs font
-        sqlite.dev
       ];
     };
 }
